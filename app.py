417f7ab0c5a58f18d97fa4f5c57a05d4235b4c3a
Your Name <your.email@example.com>
2025-02-19 01:19:10 -0800

    second commit

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ def show_submitform():
     name = request.form.get('name')
     email = request.form.get('email')
     gender = request.form.get('gender')
-    return render_template('successform.html',name = name,email = email,gender = gender)
+    mobile = request.form.get('mobile')
+    return render_template('successform.html',name = name,email = email,gender = gender,mobile=mobile)
 
 if __name__ == "__main__":
     app.run(debug=True)
